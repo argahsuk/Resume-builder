a72f5bc9ed57d6ce4166c99727e0173cf2b94165
argahsuk <agrakush4@gmail.com>
2025-03-28 19:38:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,238 @@
+let store;
+let targetelement
+document.querySelector('.preview').addEventListener('click',function(){
+    //canvas
+    let can=document.querySelector('#canvas');
+    can.style.display='block';
+    // name 
+     store=document.getElementById("name").value
+    console.log(store);
+    targetelement=document.getElementById('can-name');
+    targetelement.textContent=store;
+
+    // roll
+    store1=document.getElementById('roll').value
+    console.log(store1);
+    targetelement1=document.getElementById('can-roll');
+    targetelement1.textContent=store1;
+    
+    //email
+    store2=document.getElementById('email').value
+    console.log(store2);
+    targetelement2=document.getElementById('can-email');
+    targetelement2.textContent=store2;
+    
+    //course
+    store3=document.getElementById('course').value
+    console.log(store3);
+    targetelement3=document.getElementById('can-course');
+    targetelement3.textContent=store3;
+    
+    //contact
+    store4=document.getElementById('contact').value
+    console.log(store4);
+    targetelement4=document.getElementById('can-contact');
+    targetelement4.textContent=store4;
+    
+    //git-hub
+    store5=document.getElementById('git').value
+    console.log(store5);
+    document.getElementById('can-git').setAttribute('href',store5);
+    
+    //linkden
+    store6=document.getElementById('linkden').value
+    console.log(store6);
+    document.getElementById('can-linkden').setAttribute('href',store6);
+
+    store7=document.getElementById('ofemail').value
+    console.log(store7);
+    targetelement7=document.getElementById('can-ofemail');
+    targetelement7.textContent=store7;
+
+    //edu
+
+    //secondary
+    //name
+    store8=document.getElementById('secedu').value
+    console.log(store8);
+    targetelement8=document.getElementById('can-secedu');
+    targetelement8.textContent=store8;
+    // year
+    store9=document.getElementById('seceduy').value
+    console.log(store9);
+    targetelement9=document.getElementById('can-seceduy');
+    targetelement9.textContent=store9;  
+
+    //senior secondary
+    //name
+    store10=document.getElementById('sisecedu').value
+    console.log(store10);
+    targetelement10=document.getElementById('can-sisecedu');
+    targetelement10.textContent=store10;
+    // year
+    store11=document.getElementById('siseceduy').value
+    console.log(store11);
+    targetelement11=document.getElementById('can-siseceduy');
+    targetelement11.textContent=store11;  
+    
+    //bachelors
+    //name
+    store12=document.getElementById('bacedu').value
+    console.log(store12);
+    targetelement12=document.getElementById('can-bacedu');
+    targetelement12.textContent=store12;
+    // year
+    store13=document.getElementById('baceduy').value
+    console.log(store13);
+    targetelement13=document.getElementById('can-baceduy');
+    targetelement13.textContent=store13;  
+
+    //masters
+    //name
+    store14=document.getElementById('masedu').value
+    console.log(store14);
+    targetelement14=document.getElementById('can-masedu');
+    targetelement14.textContent=store14;
+    // year
+    store15=document.getElementById('maseduy').value
+    console.log(store15);
+    targetelement15=document.getElementById('can-maseduy');
+    targetelement15.textContent=store15;  
+
+    //skills
+
+    //one
+    store16=document.getElementById('skillName1').value
+    console.log(store16);
+    targetelement16=document.getElementById('can-skillName1');
+    targetelement16.textContent=store16;
+    
+    //two
+    store17=document.getElementById('skillName2').value
+    console.log(store17);
+    targetelement17=document.getElementById('can-skillName2');
+    targetelement17.textContent=store17;
+    
+    //three
+    store18=document.getElementById('skillName3').value
+    console.log(store18);
+    targetelement18=document.getElementById('can-skillName3');
+    targetelement18.textContent=store18;
+
+    //projects
+
+    //name
+
+    //one
+    store19=document.getElementById('proone').value
+    console.log(store19);
+    targetelement19=document.getElementById('can-proone');
+    targetelement19.textContent=store19;
+    
+    //two
+    store20=document.getElementById('protwo').value
+    console.log(store20);
+    targetelement20=document.getElementById('can-protwo');
+    targetelement20.textContent=store20;
+
+    //three
+    store21=document.getElementById('prothree').value
+    console.log(store21);
+    targetelement21=document.getElementById('can-prothree');
+    targetelement21.textContent=store21;
+    
+    //four
+    store22=document.getElementById('profour').value
+    console.log(store22);
+    targetelement22=document.getElementById('can-profour');
+    targetelement22.textContent=store22;
+    
+    //descriptions
+
+    //one
+    store24=document.getElementById('prodesone').value
+    console.log(store24);
+    targetelement24=document.getElementById('can-prodesone');
+    targetelement24.textContent=store24;
+    
+    //two
+    store23=document.getElementById('prodestwo').value
+    console.log(store23);
+    targetelement23=document.getElementById('can-prodestwo');
+    targetelement23.textContent=store23;
+
+    //four
+    store25=document.getElementById('prodesfour').value
+    console.log(store25);
+    targetelement25=document.getElementById('can-prodesfour');
+    targetelement25.textContent=store25;
+
+    //three
+    store26=document.getElementById('prodesthree').value
+    console.log(store26);
+    targetelement26=document.getElementById('can-prodesthree');
+    targetelement26.textContent=store26;
+    
+    //achievements
+    
+    //one
+    store27=document.getElementById('achievementone').value
+    console.log(store27);
+    targetelement27=document.getElementById('can-achievementone');
+    targetelement27.textContent=store27;
+    
+    store28=document.getElementById('achievementoney').value
+    console.log(store28);
+    targetelement28=document.getElementById('can-achievementoney');
+    targetelement28.textContent=store28;
+
+    //two
+    store29=document.getElementById('achievementtwo').value
+    console.log(store29);
+    targetelement29=document.getElementById('can-achievementtwo');
+    targetelement29.textContent=store29;
+    
+    store30=document.getElementById('achievementtwoy').value
+    console.log(store30);
+    targetelement30=document.getElementById('can-achievementtwoy');
+    targetelement30.textContent=store30;
+
+    //three
+    store31=document.getElementById('achievementthree').value
+    console.log(store31);
+    targetelement31=document.getElementById('can-achievementthree');
+    targetelement31.textContent=store31;
+    
+    store32=document.getElementById('achievementthreey').value
+    console.log(store32);
+    targetelement32=document.getElementById('can-achievementthreey');
+    targetelement32.textContent=store32;
+    
+    //four
+    store33=document.getElementById('achievementfour').value
+    console.log(store33);
+    targetelement33=document.getElementById('can-achievementfour');
+    targetelement33.textContent=store33;
+    
+    store34=document.getElementById('achievementfoury').value
+    console.log(store34);
+    targetelement34=document.getElementById('can-achievementfoury');
+    targetelement34.textContent=store34;
+
+})
+
+
+//adding downlaod option
+document.getElementById('download').addEventListener('click', function () {
+    let { jsPDF } = window.jspdf;
+    let doc = new jsPDF();
+
+    html2canvas(document.getElementById('canvas')).then((canvas) => {
+        let imgData = canvas.toDataURL('image/png');
+        let imgWidth = 212; // A4 width in mm
+        let imgHeight = 550;
+
+        doc.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+        doc.save('resume.pdf');
+    });
+})
